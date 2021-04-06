18d0044c52474048c7f41afb62613b7116c39fbc
Taylor Smith <tjsmith1513@gmail.com>
2021-04-06 16:32:15 -0400

    .

--- troutstocking/__init__.py
+++ troutstocking/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///selectedcounties.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
