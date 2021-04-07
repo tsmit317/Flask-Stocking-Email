cae6e5f51d94b2103d30bb1107a6775dc7664271
Taylor Smith <tjsmith1513@gmail.com>
2021-04-07 16:37:02 -0400

    Added datetime to only scrape current day

--- troutstocking/troutScrape.py
+++ troutstocking/troutScrape.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import date
+
 
 class StockingScrape():
 
@@ -9,18 +11,26 @@ class StockingScrape():
     def scrape_stocking_info(self, troutSoup):
         stocking_dict = {}
         temp_list = []
-        for i in troutSoup.findAll('td'):
-            temp_list.append(i.get_text(strip=True))
 
-        
-        for i in range(0, len(temp_list), 4):
-            if temp_list[i] + " County" in stocking_dict:
-                stocking_dict[temp_list[i] + " County"].append([temp_list[i+1], temp_list[i+2]])
-            else:
-                stocking_dict[temp_list[i]  + " County"] = [[temp_list[i+1], temp_list[i+2]]]
+        today =  date.today()
+        t = today.strftime("%A - %B %d, %Y")
+        dayofweek = today.strftime("%A")
 
-        stocking_dict['Date Added'] = troutSoup.find('th', class_='dateHeaderCell').get_text()
+        div_for_today = troutSoup.find('div', id= dayofweek.lower() + 'Container')
+        if  div_for_today is not None:
 
+            for i in div_for_today.findAll('td'):
+                temp_list.append(i.get_text(strip=True))
+
+            
+            for i in range(0, len(temp_list), 4):
+                if temp_list[i] + " County" in stocking_dict:
+                    stocking_dict[temp_list[i] + " County"].append([temp_list[i+1], temp_list[i+2]])
+                else:
+                    stocking_dict[temp_list[i]  + " County"] = [[temp_list[i+1], temp_list[i+2]]]
+
+            stocking_dict['Date Added'] = div_for_today.find('th', class_='dateHeaderCell').get_text()
+       
         return stocking_dict    
 
     def update_stocking(self):
@@ -33,5 +43,8 @@ class StockingScrape():
 
     def get_stocking_dict(self):
         return self.stock_dict
+
+    def test_new(self):
+        self.stock_dict = {'Caldwell County': [['WILSON CREEK', 'Delayed Harvest']],  'Date Added': 'Monday - August 12, 2021'}
 
 
