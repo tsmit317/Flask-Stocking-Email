140af71182a4387f3dcbf25f0b9c540df6d3536e
Taylor Smith <tjsmith1513@gmail.com>
2021-04-13 17:02:58 -0400

    Added secret key

--- troutstocking/__init__.py
+++ troutstocking/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///selectedcounties.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 from troutstocking import routes
