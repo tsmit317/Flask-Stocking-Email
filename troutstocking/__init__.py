92a979fa2879b9a45ca39f378983f32bfc30876f
Taylor Smith <tjsmith1513@gmail.com>
2021-04-14 13:48:56 -0400

    Moved config info to config.py

--- troutstocking/__init__.py
+++ troutstocking/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///selectedcounties.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object("troutstocking.config.DevelopmentConfig")
 
 db = SQLAlchemy(app)
 
