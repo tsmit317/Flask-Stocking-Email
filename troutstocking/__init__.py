1cb2ccf11b56b0add0d1a4654c24ccc36faf4847
Taylor Smith <tjsmith1513@gmail.com>
2021-04-26 13:09:57 -0400

    Added if statements to set ENV config

--- troutstocking/__init__.py
+++ troutstocking/__init__.py
@@ -3,7 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config.from_object("troutstocking.config.DevelopmentConfig")
+
+if app.config['ENV'] == 'production':
+    app.config.from_object("troutstocking.config.ProductionConfig")
+elif app.config['ENV'] == 'development':
+    app.config.from_object("troutstocking.config.DevelopmentConfig")
+elif app.config['ENV'] == 'testing':
+    app.config.from_object("troutstocking.config.TestingConfig")
 
 db = SQLAlchemy(app)
 
