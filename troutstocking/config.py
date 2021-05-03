a3482ae94eaed9f6bee987cfcacb3cab3a3e043d
Taylor Smith <tjsmith1513@gmail.com>
2021-05-03 10:23:22 -0400

    changed production and development db

--- troutstocking/config.py
+++ troutstocking/config.py
@@ -1,5 +1,5 @@
 import os
-import re
+
 
 class Config(object):
     DEBUG = False
@@ -7,20 +7,26 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SESSION_COOKIE = True
-    uri = os.environ.get('DATABASE_URL')
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_DATABASE_URI =  uri
+    
+    
+    SQLALCHEMY_DATABASE_URI =   os.environ.get('TROUT_STOCKING_DB_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTNIG = False
     SESSION_COOKIE = True
+    uri = os.environ.get('DATABASE_URL')
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SESSION_COOKIE = False
+    SQLALCHEMY_DATABASE_URI =   os.environ.get('TROUT_STOCKING_DB_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
     TESTNIG = True
