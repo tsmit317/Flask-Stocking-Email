336bcc616ea0560efade6484b26c201cc3e73cdf
Taylor Smith <tjsmith1513@gmail.com>
2021-05-03 10:43:39 -0400

    checking env type

--- troutstocking/__init__.py
+++ troutstocking/__init__.py
@@ -5,8 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 if app.config['ENV'] == 'production':
+    print('prod')
     app.config.from_object("troutstocking.config.ProductionConfig")
 elif app.config['ENV'] == 'development':
+    print('dev')
     app.config.from_object("troutstocking.config.DevelopmentConfig")
 elif app.config['ENV'] == 'testing':
     app.config.from_object("troutstocking.config.TestingConfig")
