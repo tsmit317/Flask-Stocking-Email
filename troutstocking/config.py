d0c391a23b89f00593f455e24bebdccb43d9437a
Taylor Smith <tjsmith1513@gmail.com>
2021-05-03 11:00:43 -0400

    .

--- troutstocking/config.py
+++ troutstocking/config.py
@@ -17,8 +17,8 @@ class ProductionConfig(Config):
     TESTNIG = False
     SESSION_COOKIE = True
     uri = os.environ.get('DATABASE_URL')
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
+    # if uri.startswith("postgres://"):
+    #     uri = uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
