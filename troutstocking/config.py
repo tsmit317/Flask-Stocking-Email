093b6ac9233800b0d22ca34596f0ded865a308d8
Taylor Smith <tjsmith1513@gmail.com>
2021-05-03 11:06:25 -0400

    heroku postrges issues

--- troutstocking/config.py
+++ troutstocking/config.py
@@ -16,10 +16,8 @@ class ProductionConfig(Config):
     DEBUG = False
     TESTNIG = False
     SESSION_COOKIE = True
-    uri = os.environ.get('DATABASE_URL')
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_DATABASE_URI = uri
+   
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
