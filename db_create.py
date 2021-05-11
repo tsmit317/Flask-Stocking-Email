801c493ade165f11605c052cbf477345268802b2
Taylor Smith <tjsmith1513@gmail.com>
2021-05-11 10:52:53 -0400

    Added delete tables function

--- db_create.py
+++ db_create.py
@@ -1,3 +1,10 @@
 from troutstocking import db
+from troutstocking.models import Email, Counties
 
-db.create_all()
+def create():
+    db.create_all()
+
+def delete_everthing():
+    db.session.query(Counties).delete()
+    db.session.query(Email).delete()
+    db.session.commit()
