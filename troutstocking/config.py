7c8fbefca434075093af489d14db9ec20273f031
Taylor Smith <tjsmith1513@gmail.com>
2021-05-11 10:53:02 -0400

    layout

--- troutstocking/config.py
+++ troutstocking/config.py
@@ -4,11 +4,8 @@ import os
 class Config(object):
     DEBUG = False
     TESTNIG = False
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SESSION_COOKIE = True
-    
-    
     SQLALCHEMY_DATABASE_URI =   os.environ.get('TROUT_STOCKING_DB_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
